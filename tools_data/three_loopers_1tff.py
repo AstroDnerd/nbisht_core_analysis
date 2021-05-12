71daf01f69bc43356b5847088cd900e68e3f6bca
luzloujv <luzljimvel10@gmail.com>
2021-05-12 13:05:37 -0400

    Object oriented code for paper for easier usage

--- tools_data/three_loopers_1tff.py
+++ tools_data/three_loopers_1tff.py
@@ -1,13 +1,15 @@
 from starter2 import *
 reload(looper)
+
 if 'looper1' not in dir():
-    file_list=glob.glob(dl.every_ten['u201'])
     looper1=looper.core_looper(directory=dl.sims['u201'])
+    file_list=glob.glob(dl.every_ten['u201'])
     for nfile,fname in enumerate(file_list):
         looper1.load_loop(fname)
         print( "File %d of %d"%(nfile,len(file_list)))
     looper1.out_prefix='u201'
     looper1.tr.sort_time()
+
 
 if 'looper2' not in dir():
     looper2=looper.core_looper(directory=dl.sims['u202'])
@@ -26,3 +28,4 @@ if 'looper3' not in dir():
         print( "File %d of %d"%(nfile,len(file_list)))
     looper3.out_prefix='u203'
     looper3.tr.sort_time()
+
