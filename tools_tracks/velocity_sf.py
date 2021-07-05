b578fa2063230eeb8039cf20b8a02fee76532ce1
David Collins <dccollins@fsu.edu>
2021-07-05 16:18:49 -0400

    Good version with longitudinal SF

--- tools_tracks/velocity_sf.py
+++ tools_tracks/velocity_sf.py
@@ -41,10 +41,11 @@ class trial():
         if do_plot:
             fig,ax=plt.subplots(1,1)
         rmin, rmax = 1./2048, 0.4
-        vmin, vmax = 0, 100
+        vmin, vmax = 0.1, 100
         nx=ny=64
         self.rbins = np.logspace(np.log10(rmin), np.log10(vmax),nx+1)
-        self.vbins = np.linspace(0, vmax, ny+1)
+        #self.vbins = np.linspace(vmin, vmax, ny+1)
+        self.vbins = np.logspace(np.log10(vmin), np.log10(vmax), ny+1)
         self.hist = np.zeros([nx,ny])
         def cen(arr):
             return 0.5*(arr[1:]+arr[:-1])
@@ -77,16 +78,16 @@ class trial():
             dv = cell_volume[asort]
 
             ms.get_central_velocity(core_id,nt)
-            if 1:
+            if 0:
                 self.lab='rel'
                 vr = ms.vr_rel[:,nt]  #the radial one, works ok
             if 0:
-                self.lab='mag'
+                self.lab='mag' 
                 vr = ms.rel_vmag[:,nt]  #testing
             if 0:
                 self.lab='cen'
                 vr = ms.cen_vmag[:,nt]  #testing
-            if 0:
+            if 1:
                 self.lab='RC'
                 vr = ms.rc_vmag[:,nt]  #testing
             vrs = vr[asort]
@@ -106,15 +107,14 @@ class trial():
 if 'do_all_plots' not in dir():
     do_all_plots = False
 
-
 #import three_loopers as TL
 import three_loopers_1tff as TL
 import sf2
 frame=0
-if 1:
+if 0:
     run1 = trial(TL.looper1)
     run1.by_frame(frame)
-    run2 = trial(TL.looper2)
+    run2 = trial(TL.looper2) 
     run2.by_frame(frame)
     run3 = trial(TL.looper3)
     run3.by_frame(frame)
@@ -122,7 +122,7 @@ if 1:
 #    run1.plot(frame)
 
 
-def plot(self,frame, my_sf2=None):
+def plot(self,frame, my_sf2=None,longorno=''):
     pdf = self.hist/(self.dv)
     pdf /= (pdf*self.dv).sum()
     fig,ax=plt.subplots(1,1)
@@ -130,66 +130,65 @@ def plot(self,frame, my_sf2=None):
     cmap.set_under('w')
     minmin = pdf[pdf>0].min()
     vr_max = max([v.max() for v in self.vr])
+    vr_min = min([ 0.1, my_sf2[1].min()])
     norm = mpl.colors.LogNorm(vmin=minmin,vmax=pdf.max())
     for r,v in zip(self.r,self.vr):
         ax.plot(r,v,c=[0.5,0.5,0.5,0.6],lw=0.1)
     for nc,rv in enumerate(zip(self.r,self.vr)):
         r,v=rv
         core_id = self.cores_used[nc]
-        ax.scatter(r[0],v[0],c='k')
-        ax.text(r[0],v[0],"%d"%core_id)
+        #ax.scatter(r[0],v[0],c='k')
+        #ax.text(r[0],v[0],"%d"%core_id)
     #ax.plot( self.rbins, [1.0]*self.rbins.size,c=[0.5]*3)
+    #ax.scatter(nar([1,2,3,4])/128,[10]*4)
     ploot=ax.pcolormesh(self.TheX, self.TheY, pdf,cmap=cmap,norm=norm,alpha=0.2)
-    axbonk(ax,yscale='linear',xscale='log', xlim=[1./2048,0.4], ylim=[0,vr_max], ylabel=r'$\sigma_{v,total}^2$',xlabel=r'$r$')
+    axbonk(ax,yscale='log',xscale='log', xlim=[1./128,0.4], ylim=[vr_min,vr_max], ylabel=r'$\sigma_{v,total}^2$',xlabel=r'$r$')
+    ax.set_yscale('symlog',linthresh=1)
+    from matplotlib.ticker import MultipleLocator
+    ml = MultipleLocator(10)
+    #ax.tick_params(axis='y',which='minor')
+    #ax.yaxis.set_minor_locator(ml)
+    ax.set_yticks( np.concatenate([ np.arange(0,1,0.1), np.arange(1,10), np.arange(10,100,10)]))
     fig.colorbar(ploot,ax=ax)
     if my_sf2 is not None:
         ax.plot(my_sf2[0],my_sf2[1],c='k')
-    ax.set_title('TAKE 3')
-    fig.savefig("%s/velocity_sf_%s_%s_hist_cXXXX_n%04d.pdf"%(dl.output_directory,self.lab, self.this_looper.out_prefix, frame))
+    outname = "%s/%svelocity_sf_%s_%s_hist_cXXXX_n%04d.pdf"%(dl.output_directory,longorno,self.lab, self.this_looper.out_prefix, frame)
+    fig.savefig(outname)
+    print(outname)
     plt.close(fig)
 
-def plot_each_line(self,frame, my_sf2=None):
-    pdf = self.hist/(self.dv)
-    ok = pdf>0
-    pdf[ok] /= (pdf[ok]*self.dv[ok]).sum()
-    fig,ax=plt.subplots(1,1)
-    cmap = copy.copy(mpl.cm.get_cmap("viridis"))
-    cmap.set_under('w')
-    minmin = pdf[pdf>0].min()
-    norm = mpl.colors.LogNorm(vmin=minmin,vmax=pdf.max())
-    ploot=ax.pcolormesh(self.TheX, self.TheY, pdf,cmap=cmap,norm=norm,alpha=0.2)
-    axbonk(ax,yscale='linear',xscale='log', xlim=[1./2048,0.4], ylim=[0,10], ylabel=r'$\sigma_{v,total}^2$',xlabel=r'$r$')
-    self.vr=nar(self.vr)
-    vr_max = max([v.max() for v in self.vr])
-    if my_sf2 is not None:
-        ax.plot(my_sf2[0],my_sf2[1],c='k')
-    for nc,rv in enumerate(zip(self.r,self.vr)):
-        r,v=rv
-        core_id=self.cores_used[nc]
-        ax.clear()
-        ploot=ax.pcolormesh(self.TheX, self.TheY, pdf,cmap=cmap,norm=norm,alpha=0.2,shading='nearest')
-        axbonk(ax,yscale='linear',xscale='log', xlim=[1./2048,0.4], ylim=[0,vr_max], ylabel=r'$\sigma_{v,total}^2$',xlabel=r'$r$')
-        ax.plot(my_sf2[0],my_sf2[1],c='k')
-        #ax.plot(r,v,c=[0.5,0.5,0.5,0.6],lw=0.1)
-        ax.plot(r,v,c='k')
-        ax.scatter(r[0],v[0],c='k')
-        outname = "%s/velocity_sf_%s_hist_c%04d_n%04d.png"%(dl.output_directory, self.this_looper.out_prefix, core_id,frame)
-        print(outname)
-        fig.savefig(outname)
-    #ax.plot( self.rbins, [1.0]*self.rbins.size,c=[0.5]*3)
-    fig.colorbar(ploot,ax=ax)
-    ax.set_title('TAKE 3')
-    outname="%s/velocity_sf_%s_hist_cXXXX_n%04d.pdf"%(dl.output_directory, self.this_looper.out_prefix, frame)
-    print(outname)
-    fig.savefig(outname)
-    plt.close(fig)
-import sf2
-reload( sf2)
-for this_run in [run1, run2, run3]:
-    if 'msf' not in dir() or True:
-        msf = sf2.make_sf(this_run.this_looper,0)
-        rbins,SS = msf.bin_sf2(); SS/=2*np.pi
-        #rbins,SS = msf.bin_kludged()
-    #plot_each_line(this_run,0, my_sf2=[rbins,SS])
-    plot(this_run,0, my_sf2=[rbins,SS])
-#plot(run1,0, my_sf2=[rbins,SS])
+def clean(X,Y):
+    ok = (np.isnan(Y)==False)*(Y>0)
+    return X[ok], Y[ok]
+
+if 'R' not in dir():
+    R = [[],[],[]]
+    SF2L = [[],[],[]]
+    Things = []
+    R[0], SF2L[0] = dpy('SF2_L_subset_2_u05.h5',['Rmag','SF2_L'])
+    R[1], SF2L[1] = dpy('SF2_L_subset_2_u10.h5',['Rmag','SF2_L'])
+    R[2], SF2L[2] = dpy('SF2_L_subset_2_u11.h5',['Rmag','SF2_L'])
+    SCALE = 128**6
+    SCALE = 1/128**3
+    import radial_binner as rb
+    reload(rb)
+    for n in range(3):
+        #bins = np.linspace(0,128,129)/128
+        bins = np.linspace(0,64,65)/64
+        bin_edge, bin_center, values =  rb.rb2( R[n]/128, SF2L[n], bins=bins)
+        bin_center,values = clean(bin_center,values)
+        Things.append([bin_center,values])
+
+SCALE = 128**6
+SCALE = 1/128**3
+
+
+if 1:
+    import sf2
+    reload( sf2)
+    for nrun,this_run in enumerate( [run1, run2, run3]):
+        if 'msf' not in dir() or True:
+            msf = sf2.make_sf(this_run.this_looper,0)
+            rbins,SS = msf.bin_sf2(); SS/=2*np.pi
+        #plot(this_run,0, my_sf2=[rbins,SS])
+        plot(this_run,0, my_sf2=[Things[n][0],Things[n][1]*SCALE],longorno='long')
