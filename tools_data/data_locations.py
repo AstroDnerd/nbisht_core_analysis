03e6ec39c5a02eb7e69050dfe00617c6829e421d
dcollins4096 <dcollins4096@gmail.com>
2021-08-06 14:44:42 -0400

    Update data locations to point to fixed u301 coreset

--- tools_data/data_locations.py
+++ tools_data/data_locations.py
@@ -52,7 +52,7 @@ if machine == 'Nazare':
     u201_every_ten = None
     u202_every_ten = None
     u203_every_ten = None
-    mountain_top = {'u301':"/scratch1/dcollins/Paper19/Datasets/mountain_tops/u301_new_tracks_take_9b.h5",
+    mountain_top = {'u301':"/scratch1/dcollins/Paper19/Datasets/mountain_tops/u301_new_tracks_take_9c.h5",
                     "u302":"/scratch1/dcollins/Paper19/Datasets/mountain_tops/u302_new_tracks_take_9b.h5",
                     "u303":"/scratch1/dcollins/Paper19/Datasets/mountain_tops/u303_new_tracks_take_9b.h5"}
 elif machine == 'cloudbreak':
@@ -87,7 +87,7 @@ elif machine == 'cloudbreak':
     u202_every_ten = "/data/cb1/Projects/P19_CoreSimulations/CoreSets/u202/*h5"
     u203_every_ten = "/data/cb1/Projects/P19_CoreSimulations/CoreSets/u203/*h5"
     every_ten_compact_base = "/data/cb1/Projects/P19_CoreSimulations/CoreSets/every_ten_u20x_compact"
-    mountain_top = {'u301':"/data/cb1/Projects/P19_CoreSimulations/CoreSets/mountain_tops/u301_new_tracks_take_9b.h5",
+    mountain_top = {'u301':"/data/cb1/Projects/P19_CoreSimulations/CoreSets/mountain_tops/u301_new_tracks_take_9c.h5",
                     "u302":"/data/cb1/Projects/P19_CoreSimulations/CoreSets/mountain_tops/u302_new_tracks_take_9b.h5",
                     "u303":"/data/cb1/Projects/P19_CoreSimulations/CoreSets/mountain_tops/u303_new_tracks_take_9b.h5"}
 else:
