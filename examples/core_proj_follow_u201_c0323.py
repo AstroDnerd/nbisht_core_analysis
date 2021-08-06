2c236b7653841e81a044747728d5f5999f320d7a
dcollins4096 <dcollins4096@gmail.com>
2021-08-06 15:55:44 -0400

    core proj c0323 tweaks

--- examples/core_proj_follow_u201_c0323.py
+++ examples/core_proj_follow_u201_c0323.py
@@ -8,21 +8,20 @@ reload(looper)
 reload(loop_apps)
 reload(loop_tools)
 
-import three_loopers_1tff as tl
+#import three_loopers_1tff as tl
+#this_looper = tl.looper1
+import three_loopers_mountain_top as TLM
+this_simname = 'u301'
+this_looper = TLM.loops[this_simname]
 
-this_looper = tl.looper1
 
-this_simname = 'u201'
-
-output_base = "%s_cores"%this_simname
-#Cores that overlap more than 90% with u203 core 84
-core_list =  np.array([323])
-#core_list = [84, 112]
-frame_list = list(range(10))
-fields = ['x','y','z','density']
-derived = []
-core_323_fname = 'u201_c0323_T1.h5'
 if 'this_looper' not in dir():
+    output_base = "%s_cores"%this_simname
+    core_list =  np.array([323])
+    frame_list = list(range(10))
+    fields = ['x','y','z','density']
+    derived = []
+    core_323_fname = 'u201_c0323_T1.h5'
     #for making
     this_looper = looper.core_looper(directory= dl.sims[this_simname],
                                      sim_name = this_simname,
@@ -39,29 +38,16 @@ if 'this_looper' not in dir():
     #                                 bad_particle_list=dl.bad_particles.get(this_simname,None))
     this_looper.get_tracks()
     this_looper.save(core_323_fname)
-    #this_looper.make_snapshots()
-if 'this_looper' not in dir() and False:
-    file_list=[core_323_fname]
-    this_looper=looper.core_looper(directory=dl.sims['u05'])
-    this_looper.plot_directory = "/home/dccollins/PigPen"
-    for nfile,fname in enumerate(file_list):
-        this_looper.load_loop(fname)
-        print( "File %d of %d"%(nfile,len(file_list)))
-    thtr = this_looper.tr
-    this_looper.out_prefix='core_84'
-    thtr.sort_time()
 
-this_looper.plot_directory = "/home/dccollins/PigPen"
 
-if 'color_dict' not in dir():
-    color_dict={}
-    for core_id in core_list:
-        color_dict[core_id] =  np.random.random(3)
 #loop_apps.core_proj_follow(this_looper,axis_list=[0], field='PotentialField',
 #                           zoom=False, grids=False, particles=False, moving_center=True) #, frame_list=[1])
 #slab = {'zmin':zmin, 'zmax':zmax}
 #import shift_snaps
 #shift_snaps.shift_snaps(this_looper)
+
+#core_cmap = colors.make_core_cmap( this_looper.core_list)
+
 loop_apps.core_proj_follow_b(this_looper,axis_list=[0], field='density', core_list=[323], slab=False, zoom=True, only_sphere=True, center_on_sphere=True, annotate=False, frame_list = [0,40,100,125], p_size=7)
                            #zoom=False, grids=False, particles=False, moving_center=True) #, frame_list=[1])
 #loop_apps.core_proj_multiple(this_looper,axis_list=[0], field='density', color_dict=color_dict, particles=True, fields=False, core_list )#, frame_list=[31])
