548d70e7fe16b869b2009c66bcb8704bc97943e7
David Collins <dccollins@fsu.edu>
2021-08-17 14:46:14 -0400

    Tweaking volume in mass plot

--- tools_tracks/mass_tools.py
+++ tools_tracks/mass_tools.py
@@ -15,6 +15,9 @@ class mass_tool():
         self.total_mass=defaultdict(list)
         self.dof =defaultdict(list)
         self.cores_used=[]
+        self.volume=defaultdict(list)
+        self.mean_rho=defaultdict(list)
+        self.mean_rho_w=defaultdict(list)
     def run(self,core_list=None):
         dx=1./2048
         nx = 1./dx
@@ -50,8 +53,15 @@ class mass_tool():
                 self.unique_mass[core_id].append(unique_mass)
                 self.dof[core_id].append( mask2.sum())
 
+                self.volume[core_id].append(cell_volume[mask2].sum())
 
-def plot_mass_tracks(tool, ax):
+                self.mean_rho[core_id].append( (density[mask2]*cell_volume[mask2]).sum()/(cell_volume[mask2].sum()))
+                self.mean_rho_w[core_id].append( (density[mask2]).mean() )
+                #pdb.set_trace()
+
+
+
+def plot_mass_tracks(tool, ax,core_list=None):
     G=1620./(4*np.pi)
     tff_global = np.sqrt(3*np.pi/(32*G*1))
     ntimes = tool.times.size
@@ -59,21 +69,35 @@ def plot_mass_tracks(tool, ax):
     masses = np.zeros([ntimes,ncores])
     these_times = tool.times/tff_global
     outname='plots_to_sort/mass_time_heatmap_unique_particles.pdf'
+
+    core_list=[]
+    mass_list=[]
     for ncore,core_id in enumerate(tool.cores_used):
         this_mass = tool.unique_mass[core_id]
         if len(this_mass) == 0:
             pdb.set_trace()
         masses[:,ncore]=this_mass/nar(this_mass[:6]).mean()
+        if (masses[:,ncore] > 10).any():
+            mass_list.append(ncore)
+            core_list.append(core_id)
+        #if ((masses[:,ncore] > 10)).any():
+        #    core_list.append(core_id)
+        #    mass_list.append(ncore)
+
         dof = nar(tool.dof[core_id])
         #masses[:,ncore] *= dof[0]/dof
         if np.isnan(masses).any():
             print("NANN")
             pdb.set_trace()
     nc = len(tool.cores_used)
+    print("Cores", core_list)
     number_of_lines = min([10, len(tool.cores_used)])
     take_a_few = ((nc-1)*np.random.random(number_of_lines)).astype('int')
-    print(take_a_few)
-    for ncore,core_id in enumerate(nar(tool.cores_used)[take_a_few]):
+    a_few = nar(tool.cores_used)[take_a_few]
+    if mass_list is not None:
+        a_few = nar(mass_list)
+    print(a_few)
+    for ncore in mass_list:
         rel_mass=masses[:,ncore]
         ax.plot(these_times, rel_mass,c=[0.5]*4)
         #axes[3].plot(these_times, tool.dof[core_id]/tool.dof[core_id][-1],c=[0.5]*4)
