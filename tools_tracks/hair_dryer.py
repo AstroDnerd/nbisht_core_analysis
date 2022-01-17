bf0648a6a693c55c06d1b53c7e743797bf353ede
David Collins <dccollins@fsu.edu>
2022-01-17 16:58:16 -0500

    hair dryer: now with extras for Figure 8

--- tools_tracks/hair_dryer.py
+++ tools_tracks/hair_dryer.py
@@ -1,4 +1,147 @@
 from starter2 import *
+import colors
+
+class hair_time():
+    def __init__(self,this_looper):
+        self.this_looper=this_looper
+        self.cores_used=[]
+        self.name = self.this_looper.out_prefix
+
+    def run(self, output_prefix='NAME',core_list=None, color_dict=None, 
+            verbose=False, los_list=[-1], external_axis=None):
+        dx=1./2048
+        nx = 1./dx
+        thtr = self.this_looper.tr
+        all_cores = np.unique(thtr.core_ids)
+        if core_list is None:
+            core_list = all_cores
+        if hasattr(core_list,'v'):
+            core_list=core_list.v #needs to not have unit.
+            core_list=core_list.astype('int')
+
+        thtr.sort_time()
+
+        tsorted = thtr.times/colors.tff
+        self.core_list=core_list
+        tmap = rainbow_map( len(thtr.frames))
+        mini_scrubbers = {}
+        this_looper=self.this_looper
+        if verbose:
+            print("MS 1")
+        for core_id in core_list:
+            mini_scrubbers[core_id]=  trackage.mini_scrubber(this_looper.tr,core_id, do_velocity=False)
+            mini_scrubbers[core_id].make_floats(core_id)
+
+        nparticles = nar([mini_scrubbers[core_id].nparticles for core_id in core_list])
+        from_biggest = np.argsort(nparticles)[::-1] #from biggest to smallest
+        sorted_core_list = nar(core_list)[from_biggest]
+
+        if len(los_list) > 1 or los_list[0]==-1:
+            axistag='xyz'
+        else:
+            axistag='xyz'[los_list[0]]
+        if external_axis is None:
+            if len(los_list) > 1 or los_list[0]==-1:
+                fig,ax=plt.subplots(2,2, figsize=(12,12))
+            else:
+                fig,ax=plt.subplots(1,1, figsize=(12,12))
+        else:
+            ax=external_axis
+
+
+        multiplot=True
+        if los_list[-1] == -1:
+            LOS_actual = [0,1,2,3]
+        else:
+            LOS_actual = los_list
+            multiplot=False
+        for LOS in LOS_actual:
+
+            #x = [1,0,0][LOS]
+            #y = [2,2,1][LOS]
+            #xlab=r'$%s \rm(code\ length)$'%'xyz'[x]
+            #ylab=r'$%s \rm(code\ length)$'%'xyz'[y]
+            nx = LOS%2
+            ny = int(LOS//2)
+            if external_axis is not None:
+                if multiplot:
+                    this_ax = ax[nx][ny]
+                else:
+                    this_ax=ax
+            else:
+                this_ax=ax[LOS]
+            y_final=np.zeros(sorted_core_list.size)
+            if 1:
+                #every line
+                for nc,core_id in enumerate(sorted_core_list):
+                    color=color_dict[core_id]
+                    nparticles = mini_scrubbers[core_id].nparticles
+                    if nparticles > 100:
+                        #skip every 10 for the large ones.
+                        particles = np.arange( 0, nparticles, 10).astype('int')
+                    else:
+                        particles = np.arange( 0, nparticles).astype('int')
+                    if LOS == 0:
+                        quan = mini_scrubbers[core_id].float_x
+                    elif LOS == 1:
+                        quan = mini_scrubbers[core_id].float_y
+                    elif LOS == 2:
+                        quan = mini_scrubbers[core_id].float_z
+                    elif LOS == 3:
+                        quan =np.sqrt( mini_scrubbers[core_id].float_x**2+
+                                       mini_scrubbers[core_id].float_y**2+
+                                       mini_scrubbers[core_id].float_z**2)
+
+                    for ny in  particles:
+                        this_ax.plot(tsorted,quan[ny,:],linewidth=0.1, c=color)
+                    y_final[nc]=quan[:,-1].mean()
+
+                print(y_final)
+                if 0:
+                    #text position without conflict.  Take 2.
+                    text_height = 0.01
+                    argy = np.argsort(y_final)
+                    cores_sorted=sorted_core_list[argy]
+                    y_final = y_final[argy]
+                    center=y_final.mean()
+                    bottom=center-y_final.size/2*text_height
+                    for nc,core_id in enumerate( cores_sorted):
+                        my_y = y_final[nc]
+                        my_text = bottom + (nc) * text_height
+                        my_line = bottom + (nc+0.5) * text_height
+                        text_x=tsorted[-1]+0.02
+                        this_ax.text( text_x, my_text, "%d"%core_id)
+                        this_ax.plot( [tsorted[-1],text_x],[my_y,my_line],c='k')
+                if 1:
+                    #text position without conflict. Take 1
+                    text_height = 0.02
+                    argy = np.argsort(y_final)
+                    cores_sorted=sorted_core_list[argy]
+                    y_final = y_final[argy]
+                    for nc,core_id in enumerate( cores_sorted):
+                        my_y = y_final[nc]
+                        neighbor_mask = np.abs(y_final - my_y) < text_height
+                        neighbor_y = y_final[neighbor_mask]
+                        my_i = np.argmin( np.abs( neighbor_y - my_y))
+                        neighbor_center = neighbor_y.mean()
+                        neighbor_end = neighbor_center - 0.5*neighbor_mask.sum()*text_height
+                        my_text = neighbor_end + (my_i) * text_height
+                        my_line = neighbor_end + (my_i+0.5) * text_height
+                        text_x=tsorted[-1]+0.02
+                        this_ax.text( text_x, my_text, "%d"%core_id)
+                        this_ax.plot( [tsorted[-1],text_x],[my_y,my_line],c='k')
+                        print(core_id,my_i,my_y, my_line)
+
+
+
+
+        if external_axis is None:
+            if verbose:
+                print('save')
+            fig.savefig('plots_to_sort/%s_hair_time_%s.png'%(output_prefix,axistag))
+            print("SAVE Y")
+            plt.close(fig)
+
 class hair_tool():
     def __init__(self,this_looper):
         self.this_looper=this_looper
