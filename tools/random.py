76c6cd5d6d6bc0276a88e1bbd5c1f2d02228f1f1
dcollins4096 <dcollins4096@gmail.com>
2022-02-03 14:18:39 -0500

    more

--- tools/random.py
+++ tools/random.py
@@ -8,3 +8,7 @@ x,y = np.mgrid[0:1:0.1, 0:1:0.2]
 #like np.linspace.  Use a complex step size
 x,y = np.mgrid[0:100:1j, 0:100:1j]
 
+
+#For formatting latex tables:
+a=np.arange(12)
+"%s |"*a.size%tuple(a) 
