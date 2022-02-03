def63a4d25da3c01b08e6c804ace2868bdd6ca40
dcollins4096 <dcollins4096@gmail.com>
2022-02-03 14:12:05 -0500

    add a couple tools

---
+++ tools/random.py
@@ -0,0 +1,10 @@
+
+
+fig, ax = plt.subplot(2,2)
+ax0=ax[0][0]; a1=ax[0][1]; a2=ax[1][0]; a3=ax[1][1]
+
+#like np.array
+x,y = np.mgrid[0:1:0.1, 0:1:0.2]
+#like np.linspace.  Use a complex step size
+x,y = np.mgrid[0:100:1j, 0:100:1j]
+
