01ecaeafdfed6f4f2263376ec1b477cdc14e2dd6
David Collins <dccollins@fsu.edu>
2022-02-11 15:07:45 -0500

    yt names

---
+++ tools/yt_names.py
@@ -0,0 +1,9 @@
+YT_cell_volume = ('gas','cell_volume')
+YT_particle_pos_x = ('all','particle_position_x')
+YT_particle_pos_y = ('all','particle_position_y')
+YT_particle_pos_z = ('all','particle_position_z')
+YT_particle_position = ('all','particle_position')
+YT_particle_velocity = ('all','particle_velocity')
+YT_particle_index = ('all','particle_index')
+YT_density = ('gas','density')
+
