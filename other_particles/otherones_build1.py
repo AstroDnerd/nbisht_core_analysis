17d1ec4d268384445925f7956d3dbcacc0effb5a
David Collins <dccollins@fsu.edu>
2022-02-16 13:50:38 -0500

    Tweaks to otherones builder

--- other_particles/otherones_build1.py
+++ other_particles/otherones_build1.py
@@ -10,10 +10,22 @@ reload(hair_dryer)
 import looper2
 import three_loopers_tenfour as TL4
 if 'this_simname' not in dir():
-    new_simname = 'a001'
-    other_simname = 'u301'
-    this_simname = 'u401'
-    save='a001_all_particles.h5'
+    if 1:
+        new_simname = 'a001'
+        other_simname = 'u301'
+        this_simname = 'u401'
+        save='a001_all_particles.h5'
+    if 0:
+        new_simname = 'a002'
+        other_simname = 'u302'
+        this_simname = 'u402'
+        save='a002_all_particles.h5'
+    if 0:
+        new_simname = 'a003'
+        other_simname = 'u303'
+        this_simname = 'u403'
+        save='a003_all_particles.h5'
+    boxname = 'box_of_masses_%s.h5'%new_simname
     this_looper = looper2.core_looper2( directory = dl.sims[other_simname], savefile_only_trackage=save)
     print('make ms, takes about 90 sec')
     ms = trackage.mini_scrubber(this_looper.tr, 0, do_velocity=False)
@@ -42,6 +54,7 @@ if 'new_looper' not in dir() :
     import otherones
     reload(otherones)
     print('make new one')
+
     core_list=None
     #core_list=np.unique(ht[this_simname].this_looper.tr.core_ids)
 
@@ -49,35 +62,20 @@ if 'new_looper' not in dir() :
         superset = None; name='has_neighbor'
         superset = st[simname]; name='no_neighbor'
         new_looper=otherones.find_other_ones(new_simname,ht[this_simname],core_list=core_list,superset=superset)
-        outname = "otherones_%s_%s.h5"%(this_simname,name)
+        outname = "otherones_%s_%s_b.h5"%(this_simname,name)
     if not os.path.exists(outname):
         print("SAVING FILE", outname)
         tracks_read_write.save_loop_trackage_only( new_looper, outname)
+    if not os.path.exists(boxname):
+        new_looper.box.write(boxname)
 
-if 1:
-    fig,axes=plt.subplots(2,2)
-    ax=axes[0][0];ax1=axes[0][1]
-    ax2=axes[1][0]
-
-    n_particles = [(ht[this_simname].this_looper.tr.core_ids == core_id).sum() for core_id in new_looper.box.cores_used]
-
-    Mtotal, Mother =   new_looper.box.mass_total_hull, new_looper.box.mass_other_ones
-    ax2.scatter(n_particles, Mother/Mtotal)
-    axbonk(ax2,xscale='log')
-    ax.scatter(Mtotal, Mother)
-    ax.plot([Mtotal.min(),Mtotal.max()],[Mtotal.min(),Mtotal.max()],c=[0.5]*3)
-    axbonk(ax,xlabel=r'$M_{\rm{hull}}$', ylabel=r'$M_{\rm{otherones}}$', xscale='log',yscale='log')
-    ax1.scatter(Mtotal,Mother/Mtotal)
-    axbonk(ax1,xlabel=r'$M_{\rm{hull}}$', ylabel=r'$M_{\rm{otherones}}/M_{\rm{total}}$', xscale='log',yscale='linear')
-
-    fig.savefig('plots_to_sort/otherones_mass_%s.png'%new_looper.sim_name)
 
          
 if 1:
     import otherones_hair
     reload(otherones_hair)
     core_loop = TL4.loops[this_simname]
-    core_list=None
+    core_list=[323]
     #core_list =np.unique(ht[this_simname].this_looper.tr.core_ids)[:1]
     IM = otherones_hair.images(new_looper, core_loop)
     IM.run(frames=[0, core_loop.target_frame], core_list=core_list, output_prefix=this_simname)#,core_list=[3])
