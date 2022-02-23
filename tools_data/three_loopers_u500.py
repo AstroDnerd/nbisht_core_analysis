31d6cadb33d4deb596fa98b1903aa3ad1386930f
David Collins <dccollins@fsu.edu>
2022-02-23 17:21:49 -0500

    now with u503

--- tools_data/three_loopers_u500.py
+++ tools_data/three_loopers_u500.py
@@ -7,9 +7,6 @@ if 'loops' not in dir():
     loops={}
 
 for sim_name in ['u501','u502','u503']:
-    if sim_name == 'u503':
-        print("OH NO MAKE U503")
-        continue
     print('load',sim_name)
     setname = dl.coresets+"/u500/%s_all_frame_all_prim.h5"%sim_name
     loops[sim_name] = looper2.load_looper( setname)
