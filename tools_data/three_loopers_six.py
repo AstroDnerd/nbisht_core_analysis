112e8a876ddef7a2b8aa3c644e4561640b244b09
luzloujv <luzljimvel10@gmail.com>
2022-03-01 15:46:05 -0500

    weekly update, before yt 4 is installed

--- tools_data/three_loopers_six.py
+++ tools_data/three_loopers_six.py
@@ -7,11 +7,15 @@ if 'loops' not in dir():
     loops={}
 
 if 'u601' not in loops:
-    savefile = dl.coresets+"/u600/u601_every_10_all_prim.h5"
+    savefile = dl.coresets['ourset']+'u600/u601_every_10_all_prim.h5'
+    #savefile = dl.coresets+"/u600/u601_every_10_all_prim.h5"
+    print('savefile',savefile)
     loops['u601'] = looper2.load_looper(savefile)
 if 'u602' not in loops:
-    savefile =dl.coresets+ "/u600/u602_every_10_all_prim.h5"
+    savefile =dl.coresets['ourset']+ 'u600/u602_every_10_all_prim.h5'
+    #savefile =dl.coresets+"/u600/u602_every_10_all_prim.h5"
     loops['u602'] = looper2.load_looper(savefile)
 if 'u603' not in loops:
-    savefile =dl.coresets+ "/u600/u603_every_10_all_prim.h5"
+    savefile =dl.coresets['ourset']+ 'u600/u603_every_10_all_prim.h5'
+    #savefile =dl.coresets+"/u600/u603_every_10_all_prim.h5"
     loops['u603'] = looper2.load_looper(savefile)
