c8c840705cdf298f81b8e66d0a5b6ca7d48f0ea0
David Collins <dccollins@fsu.edu>
2022-03-06 14:50:12 -0500

    u500 dict

--- tools_data/three_loopers_u500.py
+++ tools_data/three_loopers_u500.py
@@ -8,7 +8,7 @@ if 'loops' not in dir():
 
 for ns,sim_name in enumerate(['u501','u502','u503']):
     print('load',sim_name)
-    setname = dl.coresets+"/u500/%s_all_frame_all_prim.h5"%sim_name
+    setname = dl.coresets['ourset']+"/u500/%s_all_frame_all_prim.h5"%sim_name
     directory = dl.sims[sim_name]
     loops[sim_name] = looper2.load_looper( setname, directory=directory)
 
