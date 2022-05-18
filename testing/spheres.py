1ca921e98643818e749cd824c3f862d45291990f
David Collins <dccollins@fsu.edu>
2022-05-18 10:35:08 -0400

    add grad phi to spheres

--- testing/spheres.py
+++ testing/spheres.py
@@ -11,47 +11,42 @@ if 0:
     sim = 'u24'; 
     directory='/scratch1/dcollins/Paper19/u24_point_periodic'
     rsphere=1./32
+if 0:
+    sim = 'u25'; 
+    directory='/scratch1/dcollins/Paper19/u25_little_big'
+    directory='/data/cb1/Projects/P19_CoreSimulations/new_sims/u25_little_big'
+    rsphere=0.25
 if 1:
     sim = 'u25'; 
     directory='/scratch1/dcollins/Paper19/u25_little_big'
+    directory='/data/cb1/Projects/P19_CoreSimulations/new_sims/u25b_little_big_2'
     rsphere=0.25
 setname = "%s/DD%04d/data%04d"
 
 frame=1
-if 'ds' not in dir():
-    ds = yt.load(setname%(directory,frame,frame))
+ds = yt.load(setname%(directory,frame,frame))
+import xtra_energy
+xtra_energy.add_energies(ds)
 
-if 1:
+if 0:
     #make some projections
     yt.ProjectionPlot(ds,0,YT_density).save('plots_to_sort/%s_n%04d'%(sim,frame))
-    yt.ProjectionPlot(ds,0,YT_potential).save('plots_to_sort/%s_n%04d'%(sim,frame))
+    yt.ProjectionPlot(ds,0,YT_gravpotential).save('plots_to_sort/%s_n%04d'%(sim,frame))
 
     ad = ds.all_data()
     print(ad['cell_mass'].sum())
 
     M = (ad['cell_mass'][ ad['density']>2]).sum()
 
-if 0:
-    #a ray from the origin
-    #Use either this or the next one
-    foot = (0.5,0.5)
-    ray = ds.ortho_ray(0,foot)
-    plt.clf()
-    xc,yc,zc=[0.5]*3
-    r = np.sqrt((ray[YT_x].v-xc)**2 + (ray[YT_y].v-yc)**2 + (ray[YT_z].v-zc)**2)
-    G = ds['GravitationalConstant']/(np.pi*4)
-    Phi_data=ray['GravPotential']
-
 if 1:
     #all the points in the sphere
     ad = ds.all_data()
     xc,yc,zc=[0.5]*3
     r = np.sqrt( (ad[YT_x].v-xc)**2+(ad[YT_y].v-yc)**2+(ad[YT_z].v-zc)**2)
-    Phi_data = ad[YT_potential]
+    Phi_data = ad[YT_potential_field]
 
 
 #analytic result
-#Dan, make sure you understand what I'm doing here.
 Phi_analytic = np.zeros_like( r)
 ok1 = r>rsphere
 Phi_analytic[ok1] = -G*M/r[ ok1]
@@ -66,14 +61,29 @@ def shifter(x,m,b):
         Phi = Phi_analytic
     return Phi*m+b
 
-from scipy.optimize import curve_fit
+if 0:
+    #plot the potential with a shift.
+    from scipy.optimize import curve_fit
 
 #fit the analytic, which is stored in the function shifter
-popt,pcov = curve_fit( shifter, r, Phi_data.v)
-plt.scatter( r, Phi_data, label='data')
+    popt,pcov = curve_fit( shifter, r, Phi_data.v)
+    plt.scatter( r, Phi_data, label='data')
 
-plt.plot( r, Phi_analytic, label='Analytic')
+    plt.plot( r, Phi_analytic, label='Analytic')
 #check out this syntax.  popt is a list, the star * expands the list into arguments.
-plt.plot( r, shifter( r, *popt), label='shift %0.1f %0.1f'%(popt[0],popt[1]))
-plt.legend(loc=0)
-plt.savefig('plots_to_sort/ray.png')
+    plt.plot( r, shifter( r, *popt), label='shift %0.1f %0.1f'%(popt[0],popt[1]))
+    plt.legend(loc=0)
+    plt.savefig('plots_to_sort/ray.png')
+
+grad_phi_2 = np.zeros_like(r)
+grad_phi_2[ok1] = (G*M/r[ok1]**2)**2
+grad_phi_2[~ok1]= (G*M*r[~ok1]/rsphere**3)**2
+grad_phi_2 /= -8*np.pi*G
+
+grad_phi_2_data = ad[YT_grav_energy]
+#mult = grad_phi_2_data.max()/grad_phi_2.max()
+fig,ax=plt.subplots(1,1)
+ax.scatter( r, grad_phi_2_data,c='k')
+ax.plot( r, grad_phi_2)
+ax.set_yscale('symlog',linthresh=1)
+fig.savefig('plots_to_sort/grad_phi_2.png')
