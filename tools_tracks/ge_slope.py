98ef682ba5d07eaec8610cbfdf4c16bf4eafc026
David Collins <dccollins@fsu.edu>
2022-05-18 09:12:34 -0400

    tweaks to GE

--- tools_tracks/ge_slope.py
+++ tools_tracks/ge_slope.py
@@ -6,17 +6,17 @@ from scipy.optimize import curve_fit
 from scipy.ndimage import gaussian_filter
 sim_list=['u501']
 
+from collections import defaultdict
+
+class phiplotter():
 def plot_phi(this_looper,core_list=None, do_plots=True):
     if core_list is None:
         core_list = np.unique(this_looper.tr.core_ids)
 
-    ge_array=[]
-    gmm_array=[]
-    alpha_rho=[]
-    alpha_gd=[]
     frame = this_looper.target_frame
     ds = this_looper.load(frame)
     xtra_energy.add_energies(ds)
+    output = defaultdict(list)
     for core_id in core_list:
         print('Potential %s %d'%(this_looper.sim_name,core_id))
 
@@ -113,16 +113,45 @@ def plot_phi(this_looper,core_list=None, do_plots=True):
         ge_total = (GE[ok_fit]*dv[ok_fit]).sum()
         mtotal = (sp['cell_mass'][ok_fit]).sum()
         gmm      = G*mtotal**2/R_KEEP
-        ge_array.append(ge_total)
-        gmm_array.append(gmm)
+        output['ge_total'].append(ge_total)
+        output['gmm'].append(gmm)
 
         if 1:
+            #store stuff
+            output['mass'].append(mtotal)
+            output['r0'].append(R_KEEP)
+
+        if 0:
             #Just fit GE
             def plain_powerlaw( x, q, r0):
                 return q*x+r0
             popt, pcov=curve_fit(plain_powerlaw, np.log10(rok), np.log10(GE[ok_fit]))
             GE_fit_line=10**plain_powerlaw(np.log10(rok), *popt)
-            alpha_gd.append(popt[0])
+            ouput['alpha_ge'].append(popt[0])
+        if 1:
+            #Better ge fit
+            def plain_powerlaw( x, q, norm):
+                return (2*q+2)*x+norm#np.log10(G*mtotal/R_KEEP)
+            popt, pcov=curve_fit(plain_powerlaw, np.log10(rok), np.log10(GE[ok_fit]))
+            A = 10**popt[1]
+            alpha = popt[0]
+            rmax = (rok).max()
+            rmin = (rok).min()
+
+            rfit = np.linspace(rmin,rmax,128)
+            rr = 0.5*(rfit[1:]+rfit[:-1])
+            dr = rfit[1:]-rfit[:-1]
+
+            GE_fit_line=10**plain_powerlaw(np.log10(rr), *popt)
+            #GE_fit_line=10**plain_powerlaw(np.log10(rok/R_KEEP), *popt)
+            output['alpha_ge'].append(alpha)
+            output['A'].append(A)
+            output['AA'].append( G**2*mtotal**2/R_KEEP**(2*alpha+2))
+
+            R0=1
+            output['analytic'].append( 4*np.pi*A/( (R0**(2*alpha+2)*(2*alpha+5)))*(rmax**(2*alpha+5)-rmin**(2*alpha+5)))
+            output['analytic2'].append( 4*np.pi*A/( (R0**(2*alpha+2)*(2*alpha+5)))*(R_KEEP**(2*alpha+5)))
+            print('wwww',output['ge_total'][-1].v, output['analytic'][-1])
 
         if 1:
             #Fit density
@@ -135,7 +164,7 @@ def plot_phi(this_looper,core_list=None, do_plots=True):
             def powerlaw_r0_rkeep( r, q, rho0):
                 return q*np.log10(r/R_KEEP)+np.log10(rho0)
             poptd, pcovd=curve_fit(powerlaw_r0_rkeep, rok, np.log10(dok))
-            alpha_rho.append(poptd[0])
+            output['alpha_rho'].append(poptd[0])
 
         #Some plotting and fitting.
         if do_plots:
@@ -146,9 +175,11 @@ def plot_phi(this_looper,core_list=None, do_plots=True):
             if 1:
                 #plot GE
                 ax0.plot(r_cen[keepers], UE)
-                ax0.scatter( r_cen[keepers][index],UE[index],c='r')
+                print("%0.2e %0.2e"%(R_KEEP, UE[index]))
                 pch.helper(h2,xb,yb,ax=ax0,transpose=False)
-                ax0.plot( rok, GE_fit_line,c='r')
+                #ax0.plot( rok, GE_fit_line,c='r')
+                ax0.plot( rr, GE_fit_line,c='r')
+                ax0.scatter( R_KEEP,UE[index],c='r')
             if 1:
                 #plot density
                 pch.helper(dhist,xbdr,ybdr,ax=ax1,transpose=False)
@@ -165,10 +196,14 @@ def plot_phi(this_looper,core_list=None, do_plots=True):
                     power=2*poptd[0]+2
                     #print('DANS POWER',power)
                     #phi_del_squ_analy = (coe*G**2*M**2*rok**(power))/R_KEEP**(2*poptd[0]+6)
+                    #horse around
+                    DanA = (coe*G**2*M**2)/R_KEEP**(2*poptd[0]+6)
+                    phi_del_squ_analy = DanA*rok**(power)
+                    output['DanA'].append( DanA)
                     alpha=poptd[0]
                     rho0=poptd[1]
                     #phi_del_squ_analy = (4*np.pi*G*rho0*R_KEEP**(-alpha)*(2*alpha+5)/(alpha+3))**2*rok**power
-                    phi_del_squ_analy = (4*np.pi*G*rho0*R_KEEP**(-alpha)*(alpha+2)/(alpha+3))**2*rok**power
+                    #phi_del_squ_analy = (4*np.pi*G*rho0*R_KEEP**(-alpha)*(alpha+2)/(alpha+3))**2*rok**power
                     ax0.plot( rok, phi_del_squ_analy ,c='g')
 
                 if 0:
@@ -200,38 +235,57 @@ def plot_phi(this_looper,core_list=None, do_plots=True):
             axbonk(ax0,xscale='log',yscale='log',xlabel='r',ylabel='grad phi sq')
             fig.savefig(outname)
             print(outname)
-    return ge_array,gmm_array, alpha_rho, alpha_gd
+    return output
 
 if 'stuff' not in dir():
     for sim in ['u503']:
         stuff={}
         all_cores=np.unique( TL.loops[sim].tr.core_ids)
         core_list=list(all_cores)
-        core_list=None
+        core_list=all_cores
         stuff[sim]=plot_phi( TL.loops[sim],core_list=core_list, do_plots=False)
 
-for sim in stuff:
-    ge,gmm1, arho, bgd=stuff[sim]
-    gmm1=nar(gmm1)
-    ge=nar(ge)
-    arho=nar(arho)
-    bgd=nar(bgd)
-    agd = (nar(bgd)-2)/2
+if 1:
+    for sim in stuff:
+        ge   =nar(stuff[sim]['ge_total'])
+        gmm1 =nar(stuff[sim]['gmm'])
+        bgd  =nar(stuff[sim]['alpha_ge'])
+        ann  =nar(stuff[sim]['analytic'])
+        A    =nar(stuff[sim]['A'])
+        mass =nar(stuff[sim]['mass'])
+        r0   =nar(stuff[sim]['r0'])
+        alpha=nar(stuff[sim]['alpha_ge'])
+        AA1   =nar(stuff[sim]['AA'])
+        AA2 = (colors.G*mass**2/r0**(2*alpha+6))/(8*np.pi)
+        AA3 =  nar(stuff[sim]['DanA'])
 
-    gmm2 = gmm1/(4*agd+10)
-    gmm2 = gmm1*(3+agd)/(5+2*agd)
+        agd = bgd #(nar(bgd)-2)/2
 
-    gmm=gmm2
-    fig,ax=plt.subplots(1,2)
-    ax0=ax[0];ax1=ax[1]
-    ax0.scatter(ge,gmm)
-    ax1.scatter(ge,nar(ge)/nar(gmm))
-    ax0.plot(ge,ge*(1/(4*(-2)+10)))
-    ax0.plot(ge,ge)
-    axbonk(ax0,xlabel='GE',ylabel='GMM/R',xscale='log',yscale='log')
-    axbonk(ax1,xlabel='GE',ylabel='GE/GMM/R', xscale='log')
-    fig.savefig('plots_to_sort/masses_%s'%sim)
-    plt.close(fig)
-    fig,ax=plt.subplots(1,1)
-    ax.hist(agd,histtype='step')
-    fig.savefig('plots_to_sort/a_from_b_hist_%s'%sim)
+        gmm2 = gmm1/(4*agd+10)
+        #gmm2 = gmm1*(3+agd)/(5+2*agd)
+
+        gmm=gmm1
+        fig,ax=plt.subplots(2,2)
+        ax0=ax[0][0];ax1=ax[0][1]
+        ax2=ax[1][0];ax3=ax[1][1]
+        ax0.scatter(ge,gmm)
+        axbonk(ax0,xlabel='GE',ylabel='GMM/R',xscale='log',yscale='log')
+        ax1.scatter(ge,nar(ge)/nar(gmm))
+        axbonk(ax1,xlabel='GE',ylabel='GE/GMM/R', xscale='log')
+        ax0.plot(ge,ge)
+
+        ax2.scatter( ge, ann)
+        ax2.plot(ge,ge)
+        axbonk(ax2,xlabel='GE',ylabel='ANN',xscale='log',yscale='log')
+
+        ann2=4*np.pi*A/(2*alpha+5)*(r0**(2*alpha+5))
+        ax3.scatter( ge,ann2)
+        axbonk(ax3,xscale='log',yscale='log',xlabel='fit',ylabel='an/fit')
+
+
+
+        fig.savefig('plots_to_sort/masses_%s'%sim)
+        plt.close(fig)
+        fig,ax=plt.subplots(1,1)
+        ax.hist(agd,histtype='step')
+        fig.savefig('plots_to_sort/a_from_b_hist_%s'%sim)
