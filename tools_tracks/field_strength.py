833604251181ee3ccc7873d6af35aa3dae8a883e
David Collins <dccollins@fsu.edu>
2022-05-31 09:09:28 -0400

    field_strength games

--- tools_tracks/field_strength.py
+++ tools_tracks/field_strength.py
@@ -32,31 +32,31 @@ class rotoboto():
             bt = np.sqrt(bx*bx+by*by+bz*bz)
             vt = np.sqrt(vx*vx+vy*vy+vz*vz)
 
-            #0,1,2 are aligned with the field and velocity.
+            #1,2,3 are aligned with the field and velocity.
             #x,y,z are aligned with the grid
-            h0x = bx/bt
-            h0y = by/bt
-            h0z = bz/bt
+            h1x = bx/bt
+            h1y = by/bt
+            h1z = bz/bt
             
             vhatx = vx/vt
             vhaty = vy/vt
             vhatz = vz/vt
 
-            h2x = h0y*vhatz - h0z*vhaty
-            h2y = h0z*vhatx - h0x*vhatz
-            h2z = h0x*vhaty - h0y*vhatx
+            h3x = h1y*vhatz - h1z*vhaty
+            h3y = h1z*vhatx - h1x*vhatz
+            h3z = h1x*vhaty - h1y*vhatx
 
-            h1x = h2y*h0z - h2z*h0y
-            h1y = h2z*h0x - h2x*h0z
-            h1z = h2x*h0y - h2y*h0x
+            h2x = h3y*h1z - h3z*h1y
+            h2y = h3z*h1x - h3x*h1z
+            h2z = h3x*h1y - h3y*h1x
 
-            B0 = h0x*bx+h0y*by+h0z*bz
             B1 = h1x*bx+h1y*by+h1z*bz
             B2 = h2x*bx+h2y*by+h2z*bz
+            B3 = h3x*bx+h3y*by+h3z*bz
+            #V1 = h1x*vx+h1y*vy+h1z*vz
+            #V2 = h2x*vx+h2y*vy+h2z*vz
+            #V3 = h3x*vx+h3y*vy+h3z*vz
 
-            print(B1/bt)
-            #print(B1)
-            #print(B2)
 
 
 
