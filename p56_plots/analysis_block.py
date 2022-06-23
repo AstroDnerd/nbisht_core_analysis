04b83af0fd2bd3bd3f19047ae7f1d1a166a63933
David Collins <dccollins@fsu.edu>
2022-06-23 13:31:59 -0400

    update analysis

--- p56_plots/analysis_block.py
+++ p56_plots/analysis_block.py
@@ -9,23 +9,19 @@ reload(CHT)
 import hair_dryer
 reload(hair_dryer)
 import stay_close
-import three_loopers_tenfour as TL4
-sim_list=['u401','u402','u403']
+import three_loopers_six as TL
+sim_list=['u601','u602','u603']
 
-import three_loopers_tenfour as TL4
-import convex_hull_tools as CHT
-reload(CHT)
-sim_list=['u401','u402','u403']
 if 'ht' not in dir() :
     ht = {}
     for this_simname in sim_list:
-        ht[this_simname] = CHT.hull_tool(TL4.loops[this_simname])
+        ht[this_simname] = CHT.hull_tool(TL.loops[this_simname])
         ht[this_simname].make_hulls()
         ht[this_simname].make_overlaps()
 if 'ct' not in dir():
     ct = {}
     for this_simname in sim_list:
-        ct[this_simname] = stay_close.close_tool( TL4.loops[this_simname])
+        ct[this_simname] = stay_close.close_tool( TL.loops[this_simname])
         ct[this_simname].make_distance()
 
 import supersets
@@ -33,5 +29,5 @@ reload(supersets)
 if 'st' not in dir():
     st={}
     for this_simname in sim_list:
-        st[this_simname] = supersets.superset( TL4.loops[this_simname], ht[this_simname])
+        st[this_simname] = supersets.superset( TL.loops[this_simname], ht[this_simname])
         st[this_simname].find()
