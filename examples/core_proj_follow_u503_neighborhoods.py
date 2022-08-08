7a87a5854e6cf904847206324935ba9fb809d8a7
dcollins4096 <dcollins4096@gmail.com>
2022-08-08 15:40:33 -0400

    camera path

--- examples/core_proj_follow_u503_neighborhoods.py
+++ examples/core_proj_follow_u503_neighborhoods.py
@@ -12,7 +12,7 @@ import three_loopers_u500 as TL5
 set_looper = TL5.loops[this_simname]
 sim_list = ['u503']
 
-if 1:
+if 0:
     import convex_hull_tools as CHT
     reload(CHT)
     if 'ht' not in dir() :
@@ -56,19 +56,26 @@ if 1:
     reload(movie_frames)
     movie_mask = movie_frames.quantized_mask(set_looper)
     frame_list=set_looper.tr.frames[movie_mask]
-    frame_list = frame_list[ frame_list > 71]
+core_list = [52,53,55,56,57,58,74,76,77,78]
 color_dict = colors.make_core_cmap(core_list, cmap = 'tab20c', seed = -1)
+color_dict = dict([(d,'r') for d in core_list])
+import camera_path
+reload(camera_path)
 
+camera = camera_path.camera_1( set_looper, 'tight_float')
+#camera = camera_path.camera_1( set_looper, 'domain')
+#camera = camera_path.camera_1( set_looper, 'smooth_zoom')
+frame_list=frame_list[-1:]
 print('hey')
 if 1:
     print('wut')
-    mono=core_proj.core_proj_multiple(set_looper,axis_list=[2], 
+    mono=core_proj.core_proj_multiple(set_looper,camera,axis_list=[2], 
                                  color_dict=color_dict,
                                  frame_list = frame_list,
                                  field='density', 
                                  core_list=core_list,
                                  slab=False, zoom=True, only_sphere=False, center_on_sphere=True, 
-                                 annotate=True,  plot_particles=True,
+                                 annotate=True,  plot_particles=False,
                                  grids=False, float_positions=True, monotonic=True, 
                                       verbose=True,
                                       path_only=False, 
