e5e9898df19ef9b7bfcd530a8255fa040971c5c1
David Collins <dccollins@fsu.edu>
2022-09-15 12:07:12 -0400

    add buddy hair

--- browser_plots/buddy_hair.py
+++ browser_plots/buddy_hair.py
@@ -22,8 +22,10 @@ def buddy_hair(this_looper,core_list=None, suffix='', color_dict={}, what_to_plo
     y_mean_ext=extents()
     z_mean_ext=extents()
     mini_scrubbers={}
-    times = thtr.times
+    times = (thtr.times+0)/colors.tff
     times.shape = times.size,1
+    ds = this_looper.load(0)
+    ycoord=[]
     for core_id in core_list:
         ms = trackage.mini_scrubber(thtr,core_id, do_velocity=False)
         ms.particle_pos(core_id)
@@ -46,6 +48,7 @@ def buddy_hair(this_looper,core_list=None, suffix='', color_dict={}, what_to_plo
         #ax.set_aspect('equal')
 
         
+        ycoord=[]
         for ncore,core_id in enumerate(core_list):
             ms=mini_scrubbers[core_id]
             
@@ -53,7 +56,7 @@ def buddy_hair(this_looper,core_list=None, suffix='', color_dict={}, what_to_plo
             if ms.nparticles < 1000:
                 sl=slice(None)
                 #c=[0.5]*4
-                alpha=0.5
+                alpha=0.1
             else:
                 sl = slice(None,None,10)
                 #c=[0,0,0,0.1]
@@ -79,7 +82,9 @@ def buddy_hair(this_looper,core_list=None, suffix='', color_dict={}, what_to_plo
             elif what_to_plot == 'centroid':
                 ax.plot( pmean[x], pmean[y], c=color_base)
             elif what_to_plot == 'xyzt':
-                ax.plot( times, p[LOS], c=color_alpha)
+                ax.plot( times, p[LOS], c=color_alpha, linewidth=0.1)
+                ycoord.append(p[LOS].mean(axis=1)[-1])
+
                 dont_axbonk=True
 
 
@@ -92,10 +97,12 @@ def buddy_hair(this_looper,core_list=None, suffix='', color_dict={}, what_to_plo
             xmin,xmax = h_ext.minmax+nar([0.0,0.05])
             ymin,ymax = v_ext.minmax#+nar([-0.01,0.01])
             
-            text_x = pmean[x][0]
-            text_y = pmean[y][0]
+            #Text right by core.
+            #text_x = pmean[x][0]
+            #text_y = pmean[y][0]
             #ax.text( text_x, text_y, r'$%s$'%core_id, color=color_dict[core_id])
-            if 1:
+
+            if 0:
 
                 this_p = [q[-1,:] for q in p]
                 this_ax=ax
@@ -115,9 +122,43 @@ def buddy_hair(this_looper,core_list=None, suffix='', color_dict={}, what_to_plo
                 particle_v = pmean[y][-1]
                 text_height=0.02
 
+        vert_order = np.argsort( ycoord)[::-1]
+        for ncore,core_id in enumerate(nar(core_list)[vert_order]):
+            ms=mini_scrubbers[core_id]
+            ax.set(xlim=[times.min(),1.2*times.max()])
+
+            if 1:
+                #Use the Axis coordinates for the text.
+                Hcoord=ds.coordinates.x_axis[LOS]
+                Vcoord=ds.coordinates.y_axis[LOS]
+                text_height=0.04
+                start_y = 1-text_height
+                start_x = 1-4*text_height
+                text_y = start_y - ncore*text_height
+                text_x = start_x
+                points_axis=(text_x,text_y)
+                axis_to_data = ax.transAxes + ax.transData.inverted()
+                points_data = axis_to_data.transform(points_axis)
+                ax.text( text_x,text_y,r'$c%04d$'%core_id, transform=ax.transAxes, color=color_dict.get(core_id,'k'))
+            if 0:
+                frame_ind=-1    
+                if what_to_plot in ['hair','centroid']:
+                    P = np.stack([ms.mean_x[frame_ind], ms.mean_y[frame_ind], ms.mean_z[frame_ind]])
+                    #P += shifts[nc]
+                    #ax.scatter( P[Hcoord], P[Vcoord])
+                    ax.plot( [P[Hcoord], points_data[0]], [P[Vcoord], points_data[1]],linewidth=0.1, color=color_dict.get(core_id,'k'))
+                else:
+                    P = np.stack([ms.mean_x[frame_ind], ms.mean_y[frame_ind], ms.mean_z[frame_ind]])
+                    #P += shifts[nc]
+                    #ax.scatter( P[Hcoord], P[Vcoord])
+                    color = color_dict.get(core_id,'k')
+                    print(times.flatten()[-1], points_data, points_axis)
+                    ax.plot( [times.flatten()[-1], points_data[0]], [P[LOS], points_data[1]], color=color)
 
         if not dont_axbonk:
             axbonk(ax,xlabel='xyz [code length]'[x], ylabel='xyz [code length]'[y], xlim=[xmin,xmax],ylim=[ymin,ymax])
+        else:
+            ax.set( xlabel=r'$t/t_{ff}$', ylabel = 'xyz'[LOS])
 
     outname='plots_to_sort/%s_buddies__%s_%s_%s.png'%(this_looper.sim_name, what_to_plot,'xyz',suffix)
     fig.tight_layout()
