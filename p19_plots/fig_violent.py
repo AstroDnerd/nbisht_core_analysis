c997f2f471499c0b14b11d99a92cefa989e83c95
David Collins <dccollins@fsu.edu>
2023-05-23 10:58:10 -0400

    two good plots

--- p19_plots/fig_violent.py
+++ p19_plots/fig_violent.py
@@ -8,30 +8,64 @@ import pcolormesh_helper as pch
 import colors
 import movie_frames 
 G = colors.G
-def plotmancer(obj, tsing):
-        core_id=obj.core_id
+plt.close('all')
+def plotsa(accreter, tsing):
+
+
+    fig, ax=plt.subplots(1,1)
+
+    for core_id in accreter['rates']:
+        ax.plot( accreter['time']/tsing.tsing_core[core_id], accreter['rates'][core_id])
+    fig.savefig('plots_to_sort/doop.png')
+    plt.close(fig)
+
+    fig, ax=plt.subplots(1,1)
+    #ax0=ax[0]; ax1=ax[1]
+    ax0=ax
+
+    for core_id in accreter['rates']:
+        ax0.plot( accreter['time']/tsing.tsing_core[core_id], accreter['r10k'][core_id], c=[0.5]*4)
+        #ax0.plot( accreter['time'], accreter['r10k'][core_id])
+    ax0.set(yscale='log',ylabel='R [AU]', xlabel='t/tsing', xlim=[0,2])
+    m10k = nar(sorted(accreter['mass_10k']))
+    cdf = np.arange(m10k.size)/m10k.size
+    #ax1.plot( m10k, cdf)
+    #ax1.hist(m10k)
+    #ax1.set(xlabel='Mass[Msun]',ylabel='CDF')
+
+    fig.savefig('plots_to_sort/mass_10k.png')
+
+
+
+def accretion(things, tsing):
+
+
+
+    rates={}
+    r10k = {}
+    mass_r10k=[]
+    for core_id in things:
+        #core_id=obj.core_id
+        obj = things[core_id]
         #ax6.set(xlabel='R',xscale='log',ylabel='|V_r|',yscale='log')
         #ax6.set_title('Slope at tend: %0.2f'%pfit[0])
         #ax6.plot( r_bins[1:], 4*np.pi/3*r_bins[1:]**3,'k')
-        RV = obj.RV+0
-        RVt = obj.RVt+0
-        ok = ~np.isnan(RV)
-        ext = extents(RV[ok])
-        #okt = ~np.isnan(RVt)
-        #extt = extents(RVt[ok])
+        dMdTf = obj.dMdTf+0
+        MMM = obj.MMM+0
+        ok = ~np.isnan(dMdTf)
+        ext = extents(dMdTf[ok])
+        #okt = ~np.isnan(MMM)
+        #extt = extents(MMM[ok])
         #for i in range(3):
         #    ax6[i][0].set(ylim=ext.minmax)
         #    ax6[i][1].set(ylim=extt.minmax)
         #fig6.savefig('plots_to_sort/velocity_vs_radius_%s_c%04d.png'%(this_looper.sim_name, core_id))
 
-
-
-        #fig,axes=plt.subplots(1,3,figsize=(12,3))
-        fig,axes=plt.subplots(2,2,figsize=(12,12))
-        ax0=axes[0][1]; ax1=axes[0][0]
-        ax2=axes[1][0]; ax3=axes[1][1]
-        #ax0=axes[1];ax1=axes[0];ax2=axes[2]
-        #fig.subplots_adjust(wspace=0)
+        fig,axes=plt.subplots(3,1,figsize=(4,8))
+        #fig,axes=plt.subplots(2,2,figsize=(12,12))
+        #ax0=axes[0][0]; ax1=axes[0][1] ax2=axes[1][0]; ax3=axes[1][1]
+        ax0=axes[0];ax1=axes[1];ax2=axes[2]
+        fig.subplots_adjust(hspace=0)
         rcen = 0.5*(obj.r_bins[1:]+obj.r_bins[:-1])
 
         #
@@ -40,59 +74,180 @@ def plotmancer(obj, tsing):
         #4.9 pc in AU
         rcen *= colors.length_units_au
         #5900 solar masses
-        RVt *= colors.mass_units_msun
+        MMM *= colors.mass_units_msun
 
-        XXX,YYY = np.meshgrid( obj.times.flatten(),rcen)
+        times=obj.times.flatten()
+        XXX,YYY = np.meshgrid( times,rcen)
+        ok = ~np.isnan(MMM)
+        radius_to_cut_off = 1000
+        index = np.where( rcen < radius_to_cut_off)[0].max()
+        mass_at_the_end = MMM[index,-1]
+        mass_r10k.append(mass_at_the_end)
+        fiducial=mass_at_the_end
+        Max=MMM[ok].max()
+        minner_chicken_dinner = fiducial**2/Max
+        norm_mass = mpl.colors.LogNorm(vmin=minner_chicken_dinner,vmax=Max)
+
+        #print(thalf.shape)
+        #print(obj.times.shape)
+        #print(MMM.shape)
+        thalf = 0.5*(obj.times[1:]+obj.times[:-1])
+        dt    =     (obj.times[1:]-obj.times[:-1])
+        dt.shape=1,dt.size
+        dMass = MMM[:,1:]-MMM[:,:-1]
+        Mcen  = 0.5*(MMM[:,1:]+MMM[:,:-1])
+        dMdt = dMass/dt/Mcen*colors.tff
+        ok = ~np.isnan(dMdt)
+        maxmax=np.abs(dMdt[ok]).max()
+        maxmax=4
+        norm2 = mpl.colors.Normalize(-maxmax,maxmax)
+
+        
+
+
         ax=axes
-        #norm = mpl.colors.LogNorm( RV[RV>0].min(), RV.mean())
-        ok = ~np.isnan(RV)
-        maxmax = np.abs(RV[ok]).max()
+        #norm = mpl.colors.LogNorm( dMdTf[dMdTf>0].min(), dMdTf.mean())
+        ok = ~np.isnan(dMdTf)
+        maxmax = np.abs(dMdTf[ok]).max()
         norm = mpl.colors.Normalize( -maxmax,maxmax)
         #pdb.set_trace()
         cmap=copy.copy(mpl.cm.get_cmap("seismic"))
         cmap.set_bad([0.9]*3)
 
-        ok = ~np.isnan(RVt)
+        M3 = Mcen+0
+        M3[ np.isnan(M3)]=1e9
+        M10k = np.argmin(np.abs(M3-mass_at_the_end),axis=0)
+        ind = np.arange(M3.shape[1])
+        take = np.ravel_multi_index( nar([M10k,ind]), M3.shape)
+        DDD = dMdt.flatten()[take]
+        rates[core_id]=DDD
+        ax2.plot(thalf.flatten(), DDD)
+
+        #pdb.set_trace()
+        print(M10k)
+        ax1.plot(thalf.flatten(), rcen[M10k], c='k')
+        r10k[core_id]=rcen[M10k]
+
+
+
+
+
+        plot2=ax0.pcolormesh( XXX,YYY, MMM,  norm=norm_mass, shading='nearest', cmap=cmap)
+        fig.colorbar(plot2, label=r'$M(<r)~~ [M_\odot]$',ax=ax0)
+
+        AXX = ax1
+        X2,Y2=np.meshgrid(thalf.flatten(),rcen)
+        plot3=AXX.pcolormesh(X2 ,Y2, dMdt,  norm=norm2, shading='nearest', cmap=cmap)
+        fig.colorbar(plot3,label=r'$\frac{d \ln M}{d t/tff}$', ax=AXX )
+
+        if tsing:
+            for aaa in axes.flatten():
+                aaa.axvline(  tsing.tsing_core[core_id],c='k', linewidth=0.5)
+                #ax1.axvline( tsing.tsing_core[core_id],c='k')
+                aaa.axvline(  tsing.tend_core[core_id],c='k', linewidth=0.5)
+                #ax1.axvline( tsing.tend_core[core_id],c='k')
+
+        ax0.set(yscale='log', ylabel=r'$R~~[\rm{AU}]$', xticks=[])
+        ax1.set(yscale='log',ylabel=r'$R~~[\rm{AU}]$', xticks=[], xlim=ax0.get_xlim())
+        ax2.set(yscale='linear',ylabel=r'$d ln M /dt/tff$',xlabel='t/tff', xlim=ax0.get_xlim())#,ylabel='R[AU]')
+        ax2.set_aspect(ax0.get_aspect())
+
+        fig.tight_layout()
+        fig.subplots_adjust(hspace=0)
+        fig.savefig('plots_to_sort/accretion_%s_c%04d.pdf'%(obj.sim_name, core_id))
+
+    output={'rates':rates, 'time':thalf.flatten()}
+    output['radius_to_cut_off']=1000
+    output['mass_10k']=mass_r10k
+    output['r10k']=r10k
+    return output
+def plotmancer(obj, tsing):
+        core_id=obj.core_id
+        #ax6.set(xlabel='R',xscale='log',ylabel='|V_r|',yscale='log')
+        #ax6.set_title('Slope at tend: %0.2f'%pfit[0])
+        #ax6.plot( r_bins[1:], 4*np.pi/3*r_bins[1:]**3,'k')
+        dMdTf = obj.dMdTf+0
+        MMM = obj.MMM+0
+        ok = ~np.isnan(dMdTf)
+        ext = extents(dMdTf[ok])
+        #okt = ~np.isnan(MMM)
+        #extt = extents(MMM[ok])
+        #for i in range(3):
+        #    ax6[i][0].set(ylim=ext.minmax)
+        #    ax6[i][1].set(ylim=extt.minmax)
+        #fig6.savefig('plots_to_sort/velocity_vs_radius_%s_c%04d.png'%(this_looper.sim_name, core_id))
+
+
+
+        fig,axes=plt.subplots(3,1,figsize=(4,8))
+        #fig,axes=plt.subplots(2,2,figsize=(12,12))
+        #ax0=axes[0][0]; ax1=axes[0][1] ax2=axes[1][0]; ax3=axes[1][1]
+        ax0=axes[0];ax1=axes[1];ax2=axes[2]
+        fig.subplots_adjust(hspace=0)
+        rcen = 0.5*(obj.r_bins[1:]+obj.r_bins[:-1])
+
+        #
+        # UNITS.  THERES BETTER PLACES FOR THIS.
+        #
+        #4.9 pc in AU
+        rcen *= colors.length_units_au
+        #5900 solar masses
+        MMM *= colors.mass_units_msun
+
+        XXX,YYY = np.meshgrid( obj.times.flatten(),rcen)
+        ax=axes
+        #norm = mpl.colors.LogNorm( dMdTf[dMdTf>0].min(), dMdTf.mean())
+        ok = ~np.isnan(dMdTf)
+        maxmax = np.abs(dMdTf[ok]).max()
+        norm = mpl.colors.Normalize( -maxmax,maxmax)
+        #pdb.set_trace()
+        cmap=copy.copy(mpl.cm.get_cmap("seismic"))
+        cmap.set_bad([0.9]*3)
+
+        ok = ~np.isnan(MMM)
         radius_to_cut_off = 1000
         index = np.where( rcen < radius_to_cut_off)[0].max()
-        mass_at_the_end = RVt[index,-1]
+        mass_at_the_end = MMM[index,-1]
         fiducial=mass_at_the_end
-        Max=RVt[ok].max()
+        Max=MMM[ok].max()
         minner_chicken_dinner = fiducial**2/Max
         norm_mass = mpl.colors.LogNorm(vmin=minner_chicken_dinner,vmax=Max)
 
-        plot=ax0.pcolormesh( XXX,YYY, RV,  norm=norm, shading='nearest', cmap=cmap)
-        plot2=ax1.pcolormesh( XXX,YYY, RVt,  norm=norm_mass, shading='nearest', cmap=cmap)
-        fig.colorbar(plot, label=r'$\frac{\dot{M}}{M/ t_{ff}}$',ax=ax0)
-        fig.colorbar(plot2, label=r'$M(<r) [M_\odot]$',ax=ax1)
-        ax0.set(yscale='log', xlabel='t/tff')
-        ax1.set(yscale='log', xlabel='t/tff', ylabel='R [AU]')
 
+        plot2=ax0.pcolormesh( XXX,YYY, MMM,  norm=norm_mass, shading='nearest', cmap=cmap)
+        fig.colorbar(plot2, label=r'$M(<r)~~ [M_\odot]$',ax=ax0)
+        if 0:
+            #MASS FLUX FROM VELOCITY.
+            #UNDERSTAND ME LATER.
+            plot=ax1.pcolormesh( XXX,YYY, dMdTf,  norm=norm, shading='nearest', cmap=cmap)
+            fig.colorbar(plot, label=r'$\frac{\dot{M}}{M/ t_{ff}}$',ax=ax1)
+            ax1.set(yscale='log', ylabel = r'$R~~[\rm{AU}]$')
+
+        AXX = ax1
         thalf = 0.5*(obj.times[1:]+obj.times[:-1])
         dt    =     (obj.times[1:]-obj.times[:-1])
         #print(thalf.shape)
         #print(obj.times.shape)
-        #print(RVt.shape)
+        #print(MMM.shape)
         dt.shape=1,dt.size
-        dMass = RVt[:,1:]-RVt[:,:-1]
-        Mcen  = 0.5*(RVt[:,1:]+RVt[:,:-1])
+        dMass = MMM[:,1:]-MMM[:,:-1]
+        Mcen  = 0.5*(MMM[:,1:]+MMM[:,:-1])
         dMdt = dMass/dt/Mcen*colors.tff
         ok = ~np.isnan(dMdt)
         maxmax=np.abs(dMdt[ok]).max()
-        maxmax=1
+        maxmax=4
         norm2 = mpl.colors.Normalize(-maxmax,maxmax)
         X2,Y2=np.meshgrid(thalf.flatten(),rcen)
-        plot3=ax3.pcolormesh(X2 ,Y2, dMdt,  norm=norm2, shading='nearest', cmap=cmap)
-        ax3.set(yscale='log',xlabel='t/tff')
-        fig.colorbar(plot3,label='Also Q', ax=ax3 )
+        plot3=AXX.pcolormesh(X2 ,Y2, dMdt,  norm=norm2, shading='nearest', cmap=cmap)
+        fig.colorbar(plot3,label=r'$\frac{d \ln M}{d t/tff}$', ax=AXX )
 
-        Rcen=0.5*(RV[:,1:]+RV[:,:-1])
+        Rcen=0.5*(dMdTf[:,1:]+dMdTf[:,:-1])
         fig6,ax666=plt.subplots(1,1)
         #ax666.scatter(Rcen, dMdt)
         ax666.hist(np.log(np.abs(Rcen.flatten())),histtype='step')
         ax666.hist(np.log(np.abs(dMdt.flatten())*0.10),histtype='step')
         print(dt)
-        fig6.savefig('plots_to_sort/fork.png')
+        fig6.savefig('plots_to_sort/fork_c%04d.png'%core_id)
 
 
         newcmp = mpl.colors.LinearSegmentedColormap.from_list('custom blue', 
@@ -103,9 +258,12 @@ def plotmancer(obj, tsing):
 
         norm_grav = mpl.colors.LogNorm( vmin=0.01,vmax=100)
         ok = ~np.isnan(obj.RVg)
-        plot=ax2.pcolormesh( XXX,YYY, np.abs(obj.RVg),  norm=norm_grav, shading='nearest', cmap=cmap)
-        fig.colorbar(plot,label='EG/EK', ax=ax2 )
-        ax2.set(yscale='log',xlabel='t/tff')#,ylabel='R[AU]')
+        AXX=ax2
+        plot=AXX.pcolormesh( XXX,YYY, np.abs(obj.RVg),  norm=norm_grav, shading='nearest', cmap=cmap)
+        fig.colorbar(plot,label='EG/EK', ax=AXX )
+        ax0.set(yscale='log', ylabel=r'$R~~[\rm{AU}]$', xticks=[])
+        ax2.set(yscale='log',ylabel=r'$R~~[\rm{AU}]$',xlabel='t/tff', xlim=ax0.get_xlim())#,ylabel='R[AU]')
+        ax1.set(yscale='log',ylabel=r'$R~~[\rm{AU}]$', xticks=[], xlim=ax0.get_xlim())
         #pdb.set_trace()
         
         if 0:
@@ -115,20 +273,64 @@ def plotmancer(obj, tsing):
             ax.plot(times , rrrr, c=[0.5]*3, linewidth=0.1, alpha=0.5)
         if tsing:
             for aaa in axes.flatten():
-                aaa.axvline(  tsing.tsing_core[core_id],c='k')
+                aaa.axvline(  tsing.tsing_core[core_id],c='k', linewidth=0.5)
                 #ax1.axvline( tsing.tsing_core[core_id],c='k')
-                aaa.axvline(  tsing.tend_core[core_id],c='k')
+                aaa.axvline(  tsing.tend_core[core_id],c='k', linewidth=0.5)
                 #ax1.axvline( tsing.tend_core[core_id],c='k')
 
 
         fig.tight_layout()
-        print('jerk')
-        fig.savefig('plots_to_sort/mass_flux_color_%s_c%04d.pdf'%(obj.this_looper.sim_name, core_id))
+        fig.subplots_adjust(hspace=0)
+        fig.savefig('plots_to_sort/mass_flux_color_%s_c%04d.pdf'%(obj.sim_name, core_id))
+class Relax2():
+    def __init__(self):
+        pass
+def thing_saver(things,sim):
+    fptr = h5py.File('this_mass_flux_%s.h5'%sim,'w')
+    try:
+        for core_id in things:
+            obj = things[core_id]
+            core_group=fptr.create_group('c%04d'%core_id)
+            core_group['times']=obj.times
+            core_group['core_id']=obj.core_id
+            core_group['r_bins']=obj.r_bins
+            core_group['dMdTf']=obj.dMdTf
+            core_group['MMM']=obj.MMM
+            core_group['RVg']=obj.RVg
+            core_group['divv']=obj.divv
+            core_group['meanvr']=obj.meanvr
+            core_group['sim_name']=obj.sim_name
+    except:
+        raise
+    finally:
+        fptr.close()
+
+
+def thing_reader(fname):
+    fptr=h5py.File(fname,'r')
+    bucket={}
+    try:
+        for group in fptr:
+            this_one = Relax2()
+            for quan in fptr[group]:
+                QQQ = fptr[group][quan]
+                if quan == 'sim_name':
+                    QQQ = QQQ.asstr()
+                this_one.__dict__[quan]=QQQ[()]
+            bucket[this_one.core_id] = this_one
+
+            
+    except:
+        raise
+    finally:
+        fptr.close()
+    return bucket
 class Relaxor():
     def __init__(self,this_looper):
         self.this_looper=this_looper
     def run(self,core_list=None, do_plots=True, r_inflection=None, frame_list=None, tsing=None):
         this_looper=self.this_looper
+        self.sim_name = this_looper.sim_name
 
         if core_list is None:
             core_list = np.unique(this_looper.tr.core_ids)
@@ -153,9 +355,12 @@ class Relaxor():
             r_bins = np.geomspace( 2e-4, 32/128, 32)
             self.r_bins=r_bins
             r_cen = 0.5*(r_bins[1:]+r_bins[:-1])
-            self.RV = np.zeros( [len(r_bins)-1, len(frame_list)])
-            self.RVt = np.zeros( [len(r_bins)-1, len(frame_list)])
+            self.dMdTf = np.zeros( [len(r_bins)-1, len(frame_list)])
+            self.dMdT3d = np.zeros( [len(r_bins)-1, len(frame_list)])
+            self.MMM = np.zeros( [len(r_bins)-1, len(frame_list)])
             self.RVg = np.zeros( [len(r_bins)-1, len(frame_list)])
+            self.divv=np.zeros_like(self.MMM)
+            self.meanvr=np.zeros_like(self.MMM)
             frame_rmap=rainbow_map(len(frame_list))
             fig6,ax6=plt.subplots(3,2)
             ax_index=0
@@ -197,6 +402,7 @@ class Relaxor():
                 dv = scrub.cell_volume
                 RR = scrub.r
                 DD = scrub.density
+                divv = sp['velocity_divergence']
                 #dv = np.abs(sp[YT_cell_volume])
                 #RR =sp[YT_radius]
                 #DD = sp[YT_density]
@@ -225,13 +431,33 @@ class Relaxor():
                 EGmeans =nar([ EG_cuml[ digitized == i].mean() if (digitized==i).any() else -4000 for i in range(1,len(r_bins))])
                 EKmeans =nar([ EK_cuml[ digitized == i].mean() if (digitized==i).any() else -4000 for i in range(1,len(r_bins))])
 
+
+
+                divv_cuml = np.cumsum(divv[ORDER]*V_local)
+                total_vr  =nar([ vr[ digitized == i].sum() if (digitized==i).any() else np.nan for i in range(1,len(r_bins))])
+                total_divv  =nar([ divv_cuml[ digitized == i].mean() if (digitized==i).any() else np.nan for i in range(1,len(r_bins))])
+
+                mass_flux_3d = np.cumsum(-(DD[ORDER]*divv[ORDER]*dv[ORDER]))
+                F_3d  =nar([ mass_flux_3d[ digitized == i].mean() if (digitized==i).any() else np.nan for i in range(1,len(r_bins))])
+                ok = ~np.isnan(F_3d)
+                self.dMdT3d[ok,nframe] = F_3d[ok]/mass_quant[ok]*colors.tff
+
+                ok = ~np.isnan(total_vr)
+                self.meanvr[ok,nframe]=total_vr[ok]
+                self.meanvr[~ok,nframe]=np.nan
+                ok = ~np.isnan(total_divv)
+                self.divv[ok,nframe]=total_divv[ok]
+                self.divv[~ok,nframe]=np.nan
+
+
+
                 ok = ~np.isnan(mean_flux)
-                self.RV[ok,nframe]=mean_flux[ok]/mass_quant[ok]*colors.tff
-                self.RV[~ok,nframe]=np.nan
+                self.dMdTf[ok,nframe]=mean_flux[ok]/mass_quant[ok]*colors.tff
+                self.dMdTf[~ok,nframe]=np.nan
 
                 ok = ~np.isnan(mass_quant)
-                self.RVt[ok,nframe]=mass_quant[ok]
-                self.RVt[~ok,nframe]=np.nan
+                self.MMM[ok,nframe]=mass_quant[ok]
+                self.MMM[~ok,nframe]=np.nan
 
                 ok = EKmeans>0
                 self.RVg[ok,nframe]=(EGmeans/EKmeans)[ok]
@@ -289,7 +515,27 @@ if 'tsing_tool' not in dir():
 #import anne
 #reload(anne)
 ##anne.make_inflection()
-if 'thing' not in dir():
+if 'things' not in dir():
+    things={}
+
+if 1:
+    #save and replot.
+    if 0:
+        thing_saver(things,sim)
+    if 1:
+        thing_new = thing_reader('this_mass_flux_u502.h5')
+    if 0:
+        sim='u502'
+        accreter=accretion(thing_new, tsing_tool[sim])
+    if 0:
+        plotsa(accreter,tsing_tool[sim])
+
+    if 1:
+        #paper plot.  Read off disk.
+        plotmancer( thing_new[114], tsing_tool[sim])
+
+if 0:
+    #new plots.
     for sim in sim_list:
         #all_cores=np.unique( TL.loops[sim].tr.core_ids)
         #core_list=list(all_cores)
@@ -305,14 +551,33 @@ if 'thing' not in dir():
         #core_list=[323]
         #core_list=None
         #core_list = [9]
-        #core_list = TL.loops[sim].core_by_mode['Alone']
-        #core_list=core_list[2:3]
+        core_list = TL.loops[sim].core_by_mode['Alone']
+        #core_list=core_list[4:7]
 
         core_list = [114]
-
-        thing = Relaxor( TL.loops[sim])
-        thing.run(core_list=core_list, do_plots=True, frame_list=None, tsing=tsing_tool[sim])#, r_inflection=anne.inflection[sim])
+        for core_id in core_list:
+            if core_id in things:
+                continue
+            thing = Relaxor( TL.loops[sim])
+            thing.run(core_list=[core_id], do_plots=True, frame_list=None, tsing=tsing_tool[sim])#, r_inflection=anne.inflection[sim])
+            things[core_id]=thing
 
         plotmancer(thing, tsing_tool[sim])
-if 1:
+
+    if 1:
+        thing_saver(things,sim)
+    if 0:
+        if 'accreter' not in dir() or True:
+            accreter=accretion(things, tsing_tool[sim])
+    if 0:
+        plotsa(accreter,tsing_tool[sim])
+
+    if 0:
+        for core_id in things:
+            #plotmancer(things[core_id], tsing_tool[sim])
+            calculus(things[core_id],tsing_tool[sim])
+
+if 0:
     plotmancer(thing,tsing_tool[sim])
+if 0:
+    accretion(thing,tsing_tool[sim])
