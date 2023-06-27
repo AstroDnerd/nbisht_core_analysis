9066fd9757c1656040756f1ab59387f433cfe1f0
David Collins <dccollins@fsu.edu>
2023-06-27 08:29:04 -0400

    tweak starter2.  include track info

--- starter2.py
+++ starter2.py
@@ -7,7 +7,7 @@ from starter1 import *
 #so we can import things from sub directories
 path_list= ["./tools_data", "./tools_pdf", "./testing",\
             "./tools", "./tools_tracks", "./trash","./new_targets", "./p56_plots",
-            "./other_particles"]
+            "./other_particles", "./browser"]
 for directory in path_list:
     if directory not in sys.path:
         sys.path += [directory]
@@ -15,30 +15,24 @@ for directory in path_list:
 import yt
 from yt_names import *
 
-import data_locations as dl
 
 import pyximport
 pyximport.install(setup_args={'include_dirs': np.get_include()})
 import particle_ops as particle_ops
 import particle_grid_mask as particle_grid_mask
 
-import looper
-reload(looper)
+#import looper
+#reload(looper)
 import looper2
-reload(looper2)
-
 import trackage
-reload(trackage)
 import tracks_read_write
 import tracks_read_write as trw
-reload(trw)
 from davetools import *
-import loop_apps
-reload(loop_apps)
+#import loop_apps
 
+import data_locations as dl
+import track_info
+import track_loader
 
 from collections import defaultdict
 import colors
-reload(colors)
-import r_inflection
-reload(r_inflection)
