99e9c3ab515d838c0262e10afe98f90fad6cd468
dcollins <dcollins4096@gmail.com>
2024-05-21 09:51:23 -0400

    better instructions

--- starter2.py
+++ starter2.py
@@ -4,10 +4,11 @@
 #
 
 import os
+import sys
 if not os.path.exists('dtools/starter1.py'):
     print("Need to get the submodule")
-    print("submodule init")
-    print("submodule update")
+    print("git submodule init")
+    print("git submodule update")
     sys.exit(-1)
 
 #so we can import things from sub directories
