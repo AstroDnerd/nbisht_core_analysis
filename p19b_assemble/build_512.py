c99f39a6e50704686b594ec5986eab58b2b26278
dcollins <dcollins4096@gmail.com>
2024-09-16 10:49:27 -0400

    builders

--- p19b_assemble/build_512.py
+++ p19b_assemble/build_512.py
@@ -8,25 +8,30 @@ reload(bs)
 
 fields = [('enzo','Density'), ('enzo','x-velocity'),('enzo','y-velocity'),('enzo','z-velocity'),('enzo','Bx'),('enzo','By'),('enzo','Bz')]
 
-if 'ds512' not in dir():
-    ds512  = yt.load('/anvil/scratch/x-ux454321/p78c_high_res/512/B2/RS0000/restart0000')
+if 1:
+    suite = "B2"
+    outdir = "P19B_B2"
+    setname = "/anvil/scratch/x-ux454321/p19b_high_res/IC_512/%s/512/RS0000/restart0000"%suite
+    ds512  = yt.load(setname)
     cg = ds512.covering_grid(0,[0.0]*3,[512]*3)
     print('make cg')
     stuff=[]
     for field in fields:
         print('make',field)
         stuff.append( cg[field])
-    output_directory='/anvil/scratch/x-ux454321/p78c_high_res/IC_assembler/Run512'
+    #output_directory='/anvil/scratch/x-ux454321/p19b_high_res/take1/Particles_%s'%suite
+    output_directory = "/anvil/scratch/x-ux454321/p19b_high_res/take1/%s"%outdir
+
 
 if 1:
     #build the set
-    bs.write_enzo_set(stuff[0],output_directory,field='p19b_b2_density.512')
-    bs.write_enzo_set(stuff[1],output_directory,field='p19b_b2_vx.512')
-    bs.write_enzo_set(stuff[2],output_directory,field='p19b_b2_vy.512')
-    bs.write_enzo_set(stuff[3],output_directory,field='p19b_b2_vz.512')
-    bs.write_enzo_set(stuff[4],output_directory,field='p19b_b2_bx.512',extend=0 )
-    bs.write_enzo_set(stuff[5],output_directory,field='p19b_b2_by.512',extend=1 )
-    bs.write_enzo_set(stuff[6],output_directory,field='p19b_b2_bz.512',extend=2 )
+    bs.write_enzo_set(stuff[0],output_directory,field='p19b_%s_density.512'%suite)
+    bs.write_enzo_set(stuff[1],output_directory,field='p19b_%s_vx.512'%suite)
+    bs.write_enzo_set(stuff[2],output_directory,field='p19b_%s_vy.512'%suite)
+    bs.write_enzo_set(stuff[3],output_directory,field='p19b_%s_vz.512'%suite)
+    bs.write_enzo_set(stuff[4],output_directory,field='p19b_%s_bx.512'%suite,extend=0 )
+    bs.write_enzo_set(stuff[5],output_directory,field='p19b_%s_by.512'%suite,extend=1 )
+    bs.write_enzo_set(stuff[6],output_directory,field='p19b_%s_bz.512'%suite,extend=2 )
 
 if 0:
     #DO ONE STEP.
